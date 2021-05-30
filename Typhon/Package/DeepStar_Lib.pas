a92e3377e144454fd3e4ed2c6ed9ecc1268d5539
rzy11034 <rzy11034@163.com>
2021-05-30 09:11:24 +0800

    2021-05-30 09:11

--- Typhon/Package/DeepStar_Lib.pas
+++ Typhon/Package/DeepStar_Lib.pas
@@ -8,13 +8,18 @@ unit DeepStar_Lib;
 interface
 
 uses
-  DeepStar.DSA.Linear.ArrayList, DeepStar.DSA.Linear.DoubleLinkedList, DeepStar.DSA.Linear.LinkedList, 
-  DeepStar.DSA.Linear.Queue, DeepStar.DSA.Linear.Stack, DeepStar.Math, DeepStar.DSA.Strings.KMP, 
-  DeepStar.DSA.Strings.RabinKarp, DeepStar.DSA.Tree.AVLTree, DeepStar.DSA.Tree.BalanceBinarySearchTree, 
-  DeepStar.DSA.Tree.BinarySearchTree, DeepStar.DSA.Tree.BinaryTree, DeepStar.DSA.Tree.Heap, 
-  DeepStar.DSA.Tree.PriorityQueue, DeepStar.DSA.Tree.RBTree, DeepStar.DSA.Tree.TreeMap, DeepStar.DSA.Tree.Trie, 
-  DeepStar.DSA.Hash.HashMap, DeepStar.DSA.Hash.HashSet, DeepStar.DSA.Interfaces, DeepStar.Utils, 
-  DeepStar.DSA.Tree.TreeSet, DeepStar.UString;
+  DeepStar.DSA.Linear.ArrayList, DeepStar.DSA.Linear.DoubleLinkedList, 
+  DeepStar.DSA.Linear.LinkedList, DeepStar.DSA.Linear.Queue, 
+  DeepStar.DSA.Linear.Stack, DeepStar.Math, DeepStar.DSA.Strings.KMP, 
+  DeepStar.DSA.Strings.RabinKarp, DeepStar.DSA.Tree.AVLTree, 
+  DeepStar.DSA.Tree.BalanceBinarySearchTree, 
+  DeepStar.DSA.Tree.BinarySearchTree, DeepStar.DSA.Tree.BinaryTree, 
+  DeepStar.DSA.Tree.Heap, DeepStar.DSA.Tree.PriorityQueue, 
+  DeepStar.DSA.Tree.RBTree, DeepStar.DSA.Tree.TreeMap, DeepStar.DSA.Tree.Trie, 
+  DeepStar.DSA.Hash.HashMap, DeepStar.DSA.Hash.HashSet, 
+  DeepStar.DSA.Interfaces, DeepStar.Utils, DeepStar.DSA.Tree.TreeSet, 
+  DeepStar.UString, DeepStar.SDL2, DeepStar.SDL2_gfx, DeepStar.SDL2_image, 
+  DeepStar.SDL2_mixer, DeepStar.SDL2_net, DeepStar.SDL2_ttf;
 
 implementation
 
