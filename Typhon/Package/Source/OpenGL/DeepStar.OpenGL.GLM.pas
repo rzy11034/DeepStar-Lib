41fdb7cd5958bbeaa1f697a37c60d8628d746b6c
rzy <rzy11034@163.com>
2023-11-02 18:07:36 +0800

    2023-11-02 18:00

--- Typhon/Package/Source/OpenGL/DeepStar.OpenGL.GLM.pas
+++ Typhon/Package/Source/OpenGL/DeepStar.OpenGL.GLM.pas
@@ -33,10 +33,15 @@ type
     class function Mat4_Init(aa, ab, ac, ad, ba, bb, bc, bd, ca, cb, cc, cd, da,
       db, dc, dd: single): TMat4; static;
 
+    class function Normalize(vec: TVec3): TVec3;
+
+    // 返回弧度值
+    class function Radians(degrees: single): single; static;
+
     // 返回位移矩阵
     class function Translate(m: TMat4; vec: TVec3): TMat4; static;
     // 返回旋转矩阵
-    class function Rotate(m: TMat4; degree: single; vec: TVec3): TMat4; static;
+    class function Rotate(m: TMat4; degrees: single; vec: TVec3): TMat4; static;
     // 返回缩放矩阵
     class function Scale(m: TMat4; vec: TVec3): TMat4; static;
     // 按列优先返回一个一维数组指针
@@ -82,15 +87,51 @@ begin
   Result.init_zero;
 end;
 
-class function TGLM.Ortho(left, right, bottom, top, znear, zfar: single): TMat4;
+class function TGLM.Normalize(vec: TVec3): TVec3;
+
+  //function __fun1__: TVec3;
+  //var
+  //  l: single;
+  //begin
+  //  l := Sqrt((vec.Data[0]) + (vec.Data[1]) + (vec.Data[2]));
+  //  if l = 0.0 then l := 1.0;
+  //  vec /= l;
+  //
+  //  Result := vec;
+  //end;
+
+  function __fun2__: TVec3;
+  var
+    len, oneOverLen: single;
+    res: TVec3;
+  begin
+    len := vec.length;
+
+    if len <= 0 then
+      res.init(1, 0, 0);
+
+    oneOverLen := 1 / len;
+    res.init(vec.Data[0] * oneOverLen, vec.Data[1] * oneOverLen, vec.Data[2] * oneOverLen);
+    Result := res;
+  end;
+
 begin
-  Result := Mat4_Identity;
-  Result.Data[0, 0] := 2 / (right - left);
-  Result.Data[1, 1] := 2 / (top - bottom);
-  Result.Data[2, 2] := -2 / (zfar - znear);
-  Result.Data[3, 0] := -(right + left) / (right - left);
-  Result.Data[3, 1] := -(top + bottom) / (top - bottom);
-  Result.Data[3, 2] := -(zfar + znear) / (zfar - znear);
+  Result := __fun2__;
+end;
+
+class function TGLM.Ortho(left, right, bottom, top, znear, zfar: single): TMat4;
+var
+  res:TMat4;
+begin
+  res := Mat4_Identity;
+  res.Data[0, 0] := 2 / (right - left);
+  res.Data[1, 1] := 2 / (top - bottom);
+  res.Data[2, 2] := -2 / (zfar - znear);
+  res.Data[3, 0] := -(right + left) / (right - left);
+  res.Data[3, 1] := -(top + bottom) / (top - bottom);
+  res.Data[3, 2] := -(zfar + znear) / (zfar - znear);
+
+  Result := res;
 end;
 
 class function TGLM.Perspective(fovy, aspect, znear, zfar: single): TMat4;
@@ -103,9 +144,14 @@ begin
   Result := Frustum(-right, right, -top, top, znear, zfar);
 end;
 
-class function TGLM.Rotate(m: TMat4; degree: single; vec: TVec3): TMat4;
+class function TGLM.Radians(degrees: single): single;
+begin
+  Result := DegToRad(degrees);
+end;
+
+class function TGLM.Rotate(m: TMat4; degrees: single; vec: TVec3): TMat4;
 var
-  c, s, x, y, z, theta: single;
+  c, s, x, y, z: single;
   res: Tmat4;
 begin
   //mat4 rotationMatrix(vec3 axis, float angle)
@@ -121,11 +167,10 @@ begin
   //                0.0,                                0.0,                                0.0,                                1.0);
 
   res := TGLM.Mat4_Identity;
-  //theta := DegToRad(-degree);
-  theta := degree;
-  c := Cos(theta);
-  s := Sin(theta);
+  c := Cos(degrees);
+  s := Sin(degrees);
 
+  vec := Normalize(vec);
   x := vec.Data[0];
   y := vec.Data[1];
   z := vec.Data[2];
@@ -142,7 +187,7 @@ begin
   res.Data[1, 2] := y * z * (1 - c) - x * s;
   res.Data[2, 2] := z * z * (1 - c) + c;
 
-  Result := res;
+  Result := m * res;
 end;
 
 class function TGLM.Scale(m: TMat4; vec: TVec3): TMat4;
@@ -163,9 +208,9 @@ var
 begin
   res.init_identity;
 
-  res.Data[0, 3] := vec.Data[0];
-  res.Data[1, 3] := vec.Data[1];
-  res.Data[2, 3] := vec.Data[2];
+  res.Data[0, 3] += vec.Data[0];
+  res.Data[1, 3] += vec.Data[1];
+  res.Data[2, 3] += vec.Data[2];
 
   Result := m * res;
 end;
