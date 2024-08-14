0b4bb8d7df92d046b33e2fb9888237341dd1eca0
BNC204-01 <rzy11034@163.com>
2024-08-14 16:11:40 +0800

    Signed-off-by: BNC204-01 <rzy11034@163.com>

--- CodeTyphon/Package/Source/OpenGL/DeepStar.OpenGL.Texture.pas
+++ CodeTyphon/Package/Source/OpenGL/DeepStar.OpenGL.Texture.pas
@@ -8,6 +8,7 @@ interface
 uses
   Classes,
   SysUtils,
+  Math,
   System.UITypes,
   FpImage,
   FPReadJPEG,
@@ -21,21 +22,28 @@ type
     _pixels: array of GLuint;
     _width: GLint;
     _height: GLint;
+    _inverse: boolean;
+    _useAlpha: boolean;
 
-    function __FPColorToColor(const Value: TFPColor): GLuint;
+    function __FPColorToAlphaColor(const Value: TFPColor): GLuint;
     function __GetDate: Pointer;
     function __GetHeight: GLint;
+    function __GetInverse: Boolean;
+    function __GetUseAlpha: boolean;
     function __GetWidth: GLint;
+    procedure __SetInverse(const value: Boolean);
 
   public
     constructor Create;
     destructor Destroy; override;
 
-    procedure LoadFormFile(const fileName: string);
+    procedure LoadFormFile(const fileName: string;  inverse: boolean = true);
 
     property Width: GLint read __GetWidth;
     property Height: GLint read __GetHeight;
     property Pixels: Pointer read __GetDate;
+    property UseAlpha: boolean read __GetUseAlpha;
+    property Inverse: Boolean read __GetInverse write __SetInverse;
   end;
 
 implementation
@@ -52,13 +60,13 @@ begin
   inherited Destroy;
 end;
 
-procedure TTexture.LoadFormFile(const fileName: string);
+procedure TTexture.LoadFormFile(const fileName: string; inverse: boolean);
 var
   img: TFPMemoryImage;
   readerClass: TFPCustomImageReaderClass;
   reader: TFPCustomImageReader;
   c: TFPColor;
-  y, x: integer;
+  y, x, k: integer;
   fn: AnsiString;
 begin
   fn := CrossFixFileName(fileName).ToAnsiString;
@@ -70,29 +78,55 @@ begin
   try
     img.LoadFromFile(fn, reader);
 
+    if reader is TFPReaderPNG then
+      _useAlpha := (reader as TFPReaderPNG).UseAlpha;
+
     _width := img.Width;
     _height := img.Height;
 
     SetLength(_pixels, Width * Height);
+     k := 0;
 
-    for y := Height - 1 downto 0 do
-      for x := 0 to Width - 1 do
-      begin
-        c := img.Colors[x, y];
+    if inverse then
+    begin
+      for y := Height - 1 downto 0 do
+        for x := 0 to Width - 1 do
+        begin
+          c := img.Colors[x, y];
 
-        _pixels[x + ((Height - y - 1) * Width)] := __FPColorToColor(c);
-      end;
+          _pixels[k] := __FPColorToAlphaColor(c);
+          k += 1;
+        end;
+    end
+    else
+    begin
+      for y := 0 downto Height - 1 do
+        for x := 0 to Width - 1 do
+        begin
+          c := img.Colors[x, y];
+
+          _pixels[k] := __FPColorToAlphaColor(c);
+          k += 1;
+        end;
+    end;
   finally
     reader.Free;
     img.Free;
   end;
 end;
 
-function TTexture.__FPColorToColor(const Value: TFPColor): GLuint;
+function TTexture.__FPColorToAlphaColor(const Value: TFPColor): GLuint;
+var
+  ac: TColors;
 begin
-  Result := ((Value.Red shr 8) and $FF)
-    or (Value.Green and $FF00)
-    or ((Value.Blue shl 8) and $FF0000);
+  ac := Default(TColors);
+
+  ac.A := Value.Alpha div 2 ** 8;
+  ac.R := Value.Red div 2 ** 8;
+  ac.G := Value.Green div 2 ** 8;
+  ac.B := Value.Blue div 2 ** 8;
+
+  Result := ac.Color;
 end;
 
 function TTexture.__GetDate: Pointer;
@@ -105,9 +139,42 @@ begin
   Result := _height;
 end;
 
+function TTexture.__GetInverse: Boolean;
+begin
+  Result := _inverse;
+end;
+
+function TTexture.__GetUseAlpha: boolean;
+begin
+  Result := _useAlpha;
+end;
+
 function TTexture.__GetWidth: GLint;
 begin
   Result := _width;
+end;
+
+procedure TTexture.__SetInverse(const value: Boolean);
+var
+  l, r: integer;
+  temp: GLuint;
+begin
+  if value = _inverse then Exit;
+
+  _inverse := value;
+
+  l := 0;
+  r := High(_pixels);
+
+  while l < r do
+  begin
+    temp := _pixels[l];
+    _pixels[l] := _pixels[r];
+    _pixels[r] := temp;
+
+    l += 1;
+    r -= 1;
+  end;
 end;
 
 end.
