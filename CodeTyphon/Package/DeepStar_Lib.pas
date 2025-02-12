d2237e2861cbf678ce3886f3e86bf0648ea2b402
R_WIN10 <rzy11034@163.com>
2025-02-13 00:50:11 +0800

    Signed-off-by: R_WIN10 <rzy11034@163.com>

--- CodeTyphon/Package/DeepStar_Lib.pas
+++ CodeTyphon/Package/DeepStar_Lib.pas
@@ -15,8 +15,8 @@ uses
   DeepStar.DSA.Strings.KMP, DeepStar.Utils, DeepStar.DSA.Hash.HashMap, DeepStar.DSA.Linear.ArrayList, DeepStar.OpenGL.GLAD_GL, DeepStar.OpenGL.GLFW, 
   DeepStar.OpenGL.GLM, DeepStar.OpenGL.Matrix, DeepStar.OpenGL.Vector, DeepStar.OpenGL.Camera, DeepStar.OpenGL.Shader, DeepStar.OpenGL.Texture, 
   DeepStar.OpenGL.Utils, DeepStar.OpenGL.Assimp, DeepStar.OpenGL.Mesh, DeepStar.OpenGL.Model, DeepStar.SDL2_Package.Mixer, 
-  DeepStar.SDL2_Package.Texture, DeepStar.SDL2_Package.Timer, DeepStar.SDL2_Package.Utils, DeepStar.SDL2_Package.Windows, 
-  DeepStar.SDL3_Package.Utils, DeepStar.SDL3_Package.Windows, DeepStar.SDL3_Package.Texture, DeepStar.SDL3_Package.Renderer, TyphonPackageIntf;
+  DeepStar.SDL2_Package.Texture, DeepStar.SDL2_Package.Timer, DeepStar.SDL2_Package.Utils, DeepStar.SDL2_Package.Windows, DeepStar.SDL3_Package, 
+  TyphonPackageIntf;
 
 implementation
 
---
+++ CodeTyphon/Package/Source/SDL3_Package/DeepStar.SDL3_Package.pas
@@ -0,0 +1,43 @@
+﻿unit DeepStar.SDL3_Package;
+
+{$mode objfpc}{$H+}
+{$ModeSwitch unicodestrings}{$J-}
+{$modeswitch typehelpers}
+{$ModeSwitch advancedrecords}
+{$ModeSwitch implicitfunctionspecialization}
+{$ModeSwitch anonymousfunctions}
+{$ModeSwitch functionreferences}
+{$ModeSwitch duplicatelocals}
+
+interface
+
+uses
+  Classes,
+  SysUtils,
+  Types,
+  libSDL3,
+  libSDL3_image,
+  libSDL3_ttf,
+  libSDL3_mixer,
+  System.UITypes,
+  DeepStar.Utils,
+  DeepStar.DSA.Linear.ArrayList;
+
+{$I DeepStar.SDL3_Package.Utils}
+{$I DeepStar.SDL3_Package.Windows}
+{$I DeepStar.SDL3_Package.Renderer.inc}
+{$I DeepStar.SDL3_Package.Texture}
+{$I DeepStar.SDL3_Package.Mixer}
+{$I DeepStar.SDL3_Package.Timer}
+
+implementation
+
+{$I DeepStar.SDL3_Package.Utils_impl}
+{$I DeepStar.SDL3_Package.Windows_impl}
+{$I DeepStar.SDL3_Package.Renderer_impl}
+{$I DeepStar.SDL3_Package.Texture_impl} 
+{$I DeepStar.SDL3_Package.Mixer_impl}
+{$I DeepStar.SDL3_Package.Timer_impl}
+
+end.
+
